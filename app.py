8703747a34a20c2b94ee1f92b1cc3c56da115aca
mauriciodtdt <mauriciodtdt@hotmail.com>
2021-03-11 16:37:27 +1000

    test js event

--- app.py
+++ app.py
@@ -1,26 +1,56 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, Response, jsonify, render_template
 from gevent import pywsgi
+from flask_sockets import Sockets
 #from geventwebsocket.handler import WebSocketHandler
 app = Flask(__name__)
+sockets = Sockets(app)
 
-@app.route('/getmsg/', methods=['POST'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.json()
+@app.route('/listen', methods=['POST'])
+def listen():
+  name = request.json()
+  return Response(name, mimetype='text/event-stream', content=name)
 
-    # For debugging
-    print(f"Got response:\n{name}")
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "Hello this is the main page <h1>HELLO<h1>"
-
-@app.route("/<name>", methods=['POST'])
+@app.route('/<name>')
 def user(name):
-    body_api = request.json()
-    return render_template("index.html", content=body_api)
+    return render_template("index.html", content=name)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     server = pywsgi.WSGIServer(('localhost', 5000), app)
     server.serve_forever()
+
+# Copy of http://stackoverflow.com/a/20104705
+# from flask import Flask, render_template
+# from flask_sockets import Sockets
+
+# app = Flask(__name__)
+# app.debug = True
+
+# sockets = Sockets(app)
+
+# @sockets.route('/echo')
+# def echo_socket(ws):
+#     while True:
+#         message = ws.receive()
+#         ws.send(message[::-1])
+
+# @app.route('/')
+# def hello():
+#     return 'Hello World!'
+
+# @app.route('/echo_test', methods=['GET'])
+# def echo_test():
+#     return render_template('echo_test.html')
+
+# if __name__ == '__main__':
+#     app.run()
