df37953aa40d6f12a34089d2c565096d2ea34a68
mauricio <mauriciodtdt@hotmail.com>
2021-07-12 16:32:29 +1000

    Fixed index.html

--- app.py
+++ app.py
@@ -10,25 +10,28 @@ message_by_user = {}
 message_list = []
 counter = 100
 
-##############################
-# @app.route("/")
-# def index():
-#   return render_template("index.html")
+
+@app.route("/")
+def index():
+    return f"Please append your name to the URL. e.g: {request.url_rule}/<name>"
+
 
 @app.route('/<user>')
 def user(user):
-    return render_template("index.html", content=str(user))
+    return render_template("user.html", content=str(user))
 
-############################
+
 @app.route("/<user>/listen")
 def listen(user):
-  def respond_to_client():
-    global message_list
-    message_list = message_by_user.get(user) if user in message_by_user.keys() else []
-    if message_list:
-      _data = json.dumps({"timestamp": datetime.utcnow().strftime("%b %d %Y - %H:%M:%S"), "callback_msg":message_list.pop()})
-      return f"id: 1\ndata: {_data}\nevent: online\n\n"
-  return Response(respond_to_client(), mimetype='text/event-stream')
+    def respond_to_client():
+        global message_list
+        message_list = message_by_user.get(user) if user in message_by_user.keys() else []
+        if message_list:
+            _data = json.dumps({"timestamp": datetime.utcnow().strftime("%b %d %Y - %H:%M:%S"),
+                                "callback_msg": message_list.pop()})
+            return f"id: 1\ndata: {_data}\nevent: online\n\n"
+    return Response(respond_to_client(), mimetype='text/event-stream')
+
 
 @app.route('/<user>/receive', methods=['POST'])
 def receive(user):
@@ -40,8 +43,8 @@ def receive(user):
     print(f'\nTotal messages: {len(message_list)} for user: {user}')
     return _data
 
+
 ##############################
 if __name__ == "__main__":
-#   app.run(port=5000, debug=True)
   http_server = WSGIServer(("localhost", 5001), app)
   http_server.serve_forever()
